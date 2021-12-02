fc8833eeb3bbf84e25902bf34ccff8afe8b308f2
Rodrigo Couto <r@rodg.co>
2021-12-02 06:40:58 -0300

    refactor day1 into mod

---
+++ src/day1.rs
@@ -0,0 +1,26 @@
+pub fn generator(input: &str) -> Vec<u64> {
+    input.lines().map(|line| line.parse().unwrap()).collect()
+}
+
+pub fn part_1(input: &[u64]) -> u64 {
+    let (count, _) = input.iter().cloned().fold((0, 99999), |acc, x| {
+        let (mut count, last) = acc;
+        if x > last {
+            count = count + 1
+        };
+        return (count, x);
+    });
+
+    count
+}
+
+pub fn part_2(input: &[u64]) -> u64 {
+    let mut new_input: Vec<u64> = Vec::new();
+
+    input[2..].iter().enumerate().for_each(|(i, x)| {
+        // i is index in reference of "chopped" vector
+        new_input.push(x + input[i] + input[i + 1])
+    });
+
+    part_1(&new_input)
+}
---
+++ src/template.rs
@@ -0,0 +1,15 @@
+pub fn generator(input: &str) -> Vec<u64> {
+    input
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect()
+}
+
+pub fn part_1(input: &[u64]) -> u64 {
+    input[0]
+}
+
+pub fn part_2(input: &[u64]) -> u64 {
+    input[0]
+}
+
